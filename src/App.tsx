92bd09c55958a20ae5ea973a4e5289e7f1e20c83
jxxh204 <ahhancom@gmail.com>
2024-01-01 18:11:22 +0900

    FEAT : 로그인 임시 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,49 @@
 import AppWrapper from "./components/AppWrapper";
 import Bottom from "./components/InputArea";
 
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import Save from "./components/Memo/Save";
 import Memo from "./components/Memo";
 import Temp from "./components/Memo/Temp";
 import Toggle from "./components/Toggle";
 import Search from "./components/Search";
 import { useToggleContext } from "./contexts/toggle";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 function App() {
   const context = useToggleContext();
-  const [isLogin, setIsLogin] = useState(false);
+  // const [isLogin, setIsLogin] = useState(false);
 
-  // isLogin은 login이 제대로 적용된 뒤에 다시 리팩토링하자.
-  useEffect(() => {
-    const queryString = window.location.search; // Returns:'?q=123'
-    // Further parsing:
-    const params = new URLSearchParams(queryString);
-    const code = params.get("code"); // is the number 123
-    if (code) setIsLogin(true);
-  }, []);
+  // // isLogin은 login이 제대로 적용된 뒤에 다시 리팩토링하자.
+  // useEffect(() => {
+  //   const queryString = window.location.search; // Returns:'?q=123'
+  //   // Further parsing:
+  //   const params = new URLSearchParams(queryString);
+  //   const code = params.get("code"); // is the number 123
+  //   if (code) setIsLogin(true);
+  // }, []);
 
   return (
     <AppWrapper>
       <Toggle />
 
-      {!isLogin ? <Login /> : null}
+      {/* {!isLogin ? <Login /> : null} */}
       {/* Body */}
-      {!context.toggle ? (
+      {context.toggle ? (
+        <Search></Search>
+      ) : (
         <Memo>
           <Save />
           <Temp />
         </Memo>
-      ) : (
-        <Search></Search>
       )}
 
-      {isLogin ? (
-        <Bottom>
-          <Bottom.Input />
-          <Bottom.Profile />
-        </Bottom>
-      ) : null}
+      {/* {isLogin ? ( */}
+      <Bottom>
+        <Bottom.Input />
+        <Bottom.Profile />
+      </Bottom>
+      {/* ) : null} */}
     </AppWrapper>
   );
 }
