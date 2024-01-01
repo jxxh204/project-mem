f13dff7c102e5060b4bb8c8f895eb31823bc6714
jxxh204 <ahhancom@gmail.com>
2024-01-01 17:24:28 +0900

    TEST : Search 테스트 작성
    1. 버튼  작동 확인
    2. text입력 확인

--- src/__tests__/Search.test.tsx
+++ src/__tests__/Search.test.tsx
@@ -5,32 +5,56 @@ import Save from "@/components/Memo/Save";
 import Temp from "@/components/Memo/Temp";
 import InputArea from "@/components/InputArea";
 import { render } from "@testing-library/react";
+import { ToggleProvider } from "@/contexts/toggle";
+import { SearchProvider } from "@/contexts/search";
 
 describe("<Search />", () => {
   const setup = () => {
     return render(
       <ThemeProvider theme={Theme}>
-        <SearchProvider>
-          <Save />
-          <Temp />
-          <InputArea.Input />
-        </SearchProvider>
+        <ToggleProvider>
+          <SearchProvider>
+            <Save />
+            <Temp />
+            <InputArea.Input />
+          </SearchProvider>
+        </ToggleProvider>
       </ThemeProvider>
     );
   };
-  test("Submit버튼은 text 값이 있을 경우 활성화 됩니다. ", async () => {
-    setup();
+  const 값입력 = async (value: string) => {
     const submitButton = screen.getByTestId("input-submit");
     const inputTextEl =
       screen.getByPlaceholderText("당신의 경험을 검색해주세요");
 
     expect(submitButton).toBeDisabled(); //버튼 비활성화
 
-    await fireEvent.change(inputTextEl, { target: { value: "검색 입력" } }); // 입력
-    expect(inputTextEl).toHaveValue("memo1"); // 입력확인
+    await fireEvent.change(inputTextEl, { target: { value } }); // 입력
+    expect(inputTextEl).toHaveValue("검색 입력값"); // 입력확인
+    return {
+      submitButton,
+      inputTextEl,
+    };
+  };
+  test("Submit버튼은 text 값이 있을 경우 활성화 됩니다. ", async () => {
+    setup();
+    const { submitButton, inputTextEl } = await 값입력("입력값");
     expect(submitButton).not.toBeDisabled(); // 버튼 활성화 확인
     await fireEvent.submit(submitButton); //클릭
     expect(inputTextEl).toHaveValue(""); // 입력 초기화
     expect(submitButton).toBeDisabled(); // 버튼 비활성화
+  });
+
+  test("text를 입력합니다.", async () => {
+    const { submitButton } = await 값입력("검색 입력값");
+    await fireEvent.submit(submitButton); //클릭
+    screen.getByRole("listitem", { name: "검색 입력값" });
+  });
+
+  test("입력 후 답장을 받습니다.", async () => {
+    const { submitButton } = await 값입력("검색 입력값");
+    await fireEvent.submit(submitButton); //클릭
+    screen.getByRole("listitem", { name: "검색 입력값" });
+    screen.getByRole("listitem", { name: "답장값" });
   });
 });
